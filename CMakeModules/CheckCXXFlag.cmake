90d9a9ba5717406b02ef626d5a19cf2a0f818a50
MrArborsexual <arborsexual@protonmail.ch>
2025-08-10 13:45:32 -0400

    trying to get duckstation to work with gentoo and system-libs; it does not; really you should be using this

--- CMakeModules/CheckCXXFlag.cmake
+++
@@ -1,8 +0,0 @@
-include(CheckCXXCompilerFlag)
-
-function(check_cxx_flag flag var)
-    CHECK_CXX_COMPILER_FLAG("-Werror ${flag}" ${var})
-    if(${var})
-       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" PARENT_SCOPE)
-    endif()
-endfunction()
